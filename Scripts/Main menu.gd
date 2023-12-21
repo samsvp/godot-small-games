45ed04a3a6a6b97974a14e7c774a17659d760a66
Samuel Simplicio <samuelsimplicio5@gmail.com>
2023-12-20 23:32:59 -0300

    started a bullet hell shooter

--- Scripts/Main menu.gd
+++ Scripts/Main menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	%Pong.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +16,11 @@ func _input(ev):
 			get_tree().change_scene_to_file("res://Scenes/endless_runner.tscn")
 		if ev.keycode == KEY_P:
 			get_tree().change_scene_to_file("res://Scenes/Pong/main.tscn")
+
+
+func _on_pong_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Pong/main.tscn")
+
+
+func _on_space_shooter_pressed():
+	get_tree().change_scene_to_file("res://Scenes/SpaceShooter/space_shooter_main.tscn")
